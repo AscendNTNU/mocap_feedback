0797c378803d2d8153d13579ddedcb46054236fe
sigridmellemseter <sigrimel@stud.ntnu.no>
2021-11-02 19:41:10 +0100

    Added connection to mavlink

--- src/pose_subscriber.py
+++ src/pose_subscriber.py
@@ -13,20 +13,6 @@ from rclpy.node import Node
 from geometry_msgs.mgs import PoseStamped #This is the message type of the pose 
 
 
-class PoseSubscriber(Node):
-
-    def __init__(self):
-        super().__init__('pose_subscriber')
-        self.subscription = self.create_subscription(
-            PoseStamped,
-            '/qualisys/marlin/pose geometry_msgs/PoseStamped',  #Message type and topic 
-            self.listener_callback,
-            10)
-        self.subscription  # prevent unused variable warning
-
-    def listener_callback(self, msg):
-        self.get_logger().info(msg.data)  #Here the data the node receives is printed
-
 def wait_conn():
     """
     Sends a ping to estabilish serial communication and waits for a response
@@ -43,29 +29,20 @@ def wait_conn():
         time.sleep(0.5)
         
 
-# Create the connection 
-master = mavutil.mavlink_connection("/dev/cu.usbserial-0001", baud=57600)
-wait_conn()
-print("Waiting for heartbeat")
-master.wait_heartbeat()
-print("Setting origin SET_GPS_GLOBAL_ORIGIN to initialize EKF (0,0,0)")
-master.mav.set_gps_global_origin_send(master.target_system, 0, 0, 0)
+
+#print("Setting origin SET_GPS_GLOBAL_ORIGIN to initialize EKF (0,0,0)")
 
 #
 #   Arm
 #
-print("Feeding some data")
+#print("Feeding some data")
 
 #data = master.mav.att_pos_mocap_encode(time_usec=time.time(),q=[1, 0, 0, 0],x=0,y=0,z=0)
 
-master.mav.att_pos_mocap_send(np.uint64(time.time()),[1.0, 0.0, 0.0, 0.0],0.0,0.0,0.0) #Time is epoch time
+#master.mav.att_pos_mocap_send(np.uint64(time.time()),[1.0, 0.0, 0.0, 0.0],0.0,0.0,0.0) #Time is epoch time
 
 
-
-
-
-class PoseSubscriber(Node):
-
+class PoseSubscriber(Node): 
     def __init__(self):
         super().__init__('pose_subscriber')
         self.subscription = self.create_subscription(
@@ -73,16 +50,28 @@ class PoseSubscriber(Node):
             '/qualisys/marlin/pose geometry_msgs/PoseStamped',  #Message type and topic 
             self.listener_callback,
             10)
-        self.subscription  # prevent unused variable warning
+        self.master 
+    
+    def init_connection(self):
+        self.master = mavutil.mavlink_connection("/dev/cu.usbserial-0001", baud=57600)
+        wait_conn()
+        self.master.wait_heartbeat()
+
+    def set_origin(self):
+        self.master.mav.set_gps_global_origin_send(self.master.target_system, 0, 0, 0)
 
     def listener_callback(self, msg):
         self.get_logger().info(msg.data)  #Here the data that the node receives is printed
+    
+
 
 
 def main(args=None):
     rclpy.init(args=args)
 
     pose_subscriber = PoseSubscriber()
+    pose_subscriber.init_connection()
+    #Etabler connection 
 
     rclpy.spin(pose_subscriber)
 
