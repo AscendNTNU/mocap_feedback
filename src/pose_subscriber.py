179986f124908532b0d2592860746af24d1e6e28
sigridmellemseter <90564696+sigridmellemseter@users.noreply.github.com>
2021-11-02 18:31:36 +0100

    Update pose_subscriber.py

--- src/pose_subscriber.py
+++ src/pose_subscriber.py
@@ -1,35 +1,39 @@
+#This node is listening to the topic that the pose is published to 
+#It is used to feed to pose to the drone via MAVLink 
+
 import rclpy
 from rclpy.node import Node
 
 from std_msgs.msg import String
+from geometry_msgs.mgs import PoseStamped #This is the message type of the pose 
 
 
-class MinimalSubscriber(Node):
+class PoseSubscriber(Node):
 
     def __init__(self):
         super().__init__('pose_subscriber')
         self.subscription = self.create_subscription(
-            String,
-            '/qualisys/marlin/pose geometry_msgs/PoseStamped',  #Topic 
+            PoseStamped,
+            '/qualisys/marlin/pose geometry_msgs/PoseStamped',  #Message type and topic 
             self.listener_callback,
             10)
         self.subscription  # prevent unused variable warning
 
     def listener_callback(self, msg):
-        self.get_logger().info('I heard: "%s"' % msg.data)
+        self.get_logger().info(msg.data)  #Here the data that the node receives is printed
 
 
 def main(args=None):
     rclpy.init(args=args)
 
-    minimal_subscriber = MinimalSubscriber()
+    pose_subscriber = PoseSubscriber()
 
-    rclpy.spin(minimal_subscriber)
+    rclpy.spin(pose_subscriber)
 
     # Destroy the node explicitly
     # (optional - otherwise it will be done automatically
     # when the garbage collector destroys the node object)
-    minimal_subscriber.destroy_node()
+    pose_subscriber.destroy_node()
     rclpy.shutdown()
 
 
