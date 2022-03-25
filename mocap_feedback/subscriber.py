f4a10fe0be9ec283e8c3d15ff3e352ee9d03c62a
vetleras <vetleras@gmail.com>
2022-03-25 16:13:30 +0100

    Add rate limiting

--- mocap_feedback/subscriber.py
+++ mocap_feedback/subscriber.py
@@ -7,10 +7,10 @@ import tf2_ros
 import tf2_geometry_msgs
 
 master = mavutil.mavlink_connection("udpin:0.0.0.0:14551")
+msg_transformed = PoseStamped()
 
-def callback(msg: PoseStamped):
-    #time_usec = int(msg.header.stamp.secs * 1e6 + msg.header.stamp.nsecs / 1e3)
-    
+def receive_callback(msg: PoseStamped):
+    global msg_transformed
     transform = buffer.lookup_transform(
                                        # target frame:
                                        "local_ned",
@@ -23,6 +23,8 @@ def callback(msg: PoseStamped):
 
     msg_transformed = tf2_geometry_msgs.do_transform_pose(msg, transform)
 
+def callback(_):
+    #time_usec = int(msg.header.stamp.secs * 1e6 + msg.header.stamp.nsecs / 1e3)
     time_usec = int(time.perf_counter_ns()* 1e3)
 
     o = msg_transformed.pose.orientation
@@ -33,17 +35,33 @@ def callback(msg: PoseStamped):
 
     master.mav.att_pos_mocap_send(time_usec, q, x, y, z)
 
+def wait_conn(): #Ping untill connection is established
+    msg = None
+    while not msg:
+        master.mav.ping_send(
+            int(time.time() * 1e6), # Unix time in microseconds
+            0, # Ping number
+            0, # Request ping of all systems
+            0 # Request ping of all components
+        )
+        print(".")
+        msg = master.recv_match()
+        time.sleep(0.5)
+
 
 def set_origin():
-    master.mav.set_gps_global_origin_send(master.target_system, 0, 0, 0)
+    master.mav.set_gps_global_origin_send(master.target_system, 599168569, 107284696, 0)
     master.mav.set_home_position_send(
         master.target_system, 0, 0, 0, 0, 0, 0, [0, 0, 0, 0], 0, 0, 0
     )
 
 
 rospy.init_node("mocap_feedback")
+wait_conn()
+print("Got conn")
 buffer = tf2_ros.Buffer()
 listener = tf2_ros.TransformListener(buffer)
-rospy.Subscriber("/qualisys/elvind/pose", PoseStamped, callback)
+rospy.Subscriber("/qualisys/elvind/pose", PoseStamped, receive_callback)
+rospy.Timer(rospy.Duration(0.2), callback)
 set_origin()
 rospy.spin()
