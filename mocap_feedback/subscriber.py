3e1e9d8d08dd77fddc10604a53c274788d9b2c44
vetleras <vetleras@gmail.com>
2022-03-25 14:05:54 +0100

    Add tf2

--- mocap_feedback/subscriber.py
+++ mocap_feedback/subscriber.py
@@ -1,29 +1,50 @@
 #! /usr/bin/env python
 import rospy
 from geometry_msgs.msg import PoseStamped
-import sys
 from pymavlink import mavutil
+import time
+import tf2
+import tf2_geometry_msgs
 
-i = 0  # number of messages since last message sent
-N = int(sys.argv[1])  # send every N msg, throw the rest
+master = mavutil.mavlink_connection("/dev/ttyACM2", baud=57600)
 
 
-def callback(msg):
-    global i
-    i += 1
-    if i == 2:
-        publisher.publish(msg)
-        i = 0
+
+def callback(msg: PoseStamped):
+    #time_usec = int(msg.header.stamp.secs * 1e6 + msg.header.stamp.nsecs / 1e3)
+    
+    transform = listener.lookup_transform(
+                                       # target frame:
+                                       "local_ned",
+                                       # source frame:
+                                       msg.header.frame_id,
+                                       # get the tf at the time the pose was valid
+                                       msg.header.stamp,
+                                       # wait for at most 1 second for transform, otherwise throw
+                                       rospy.Duration(1.0))
+
+    msg_transformed = tf2_geometry_msgs.do_transform_pose(msg, transform)
+
+    time_usec = int(time.perf_counter_ns()* 1e3)
+
+    o = msg_transformed.pose.orientation
+    q = [o.w, o.x, o.y, o.z]
+
+    p = msg_transformed.pose.position
+    x, y, z = p.x, p.y, p.z
+
+    master.mav.att_pos_mocap_send(time_usec, q, x, y, z)
 
 
 def set_origin():
-    master = mavutil.mavlink_connection("/dev/ttyUSB0", baud=57600)
     master.mav.set_gps_global_origin_send(master.target_system, 0, 0, 0)
-    master.mav.set_home_position_send(master.target_system, 0, 0, 0, 0)
+    master.mav.set_home_position_send(
+        master.target_system, 0, 0, 0, 0, 0, 0, [0, 0, 0, 0], 0, 0, 0
+    )
 
 
 rospy.init_node("mocap_feedback")
+listener = tf2.TransformListener()
 rospy.Subscriber("/qualisys/elvind/pose", PoseStamped, callback)
-publisher = rospy.Publisher("/mavros/vision/pose", PoseStamped, queue_size=1)
 set_origin()
 rospy.spin()
